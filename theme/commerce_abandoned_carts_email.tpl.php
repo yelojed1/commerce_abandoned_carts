679ecd152db50f21e67b26bd2f775a223e6b056e
Julien de Nas de Tourris <julien@sparksinteractive.co.nz>
2018-02-24 08:17:56 -0500

    Added email template for notifications

---
+++ theme/commerce_abandoned_carts_email.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Default implementation of the Commerce Abandoned Cart Messages email template.
+ *
+ * Available variables:
+ * - $order: the fully loaded Commerce order object
+ */
+?>
+
+Dear Valued Customer,<br />
+
+<p>This is a reminder that your orderd.</p>
+
